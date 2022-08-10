9d0dec293189c37ce26106e27973ba7313d11508
Henry Nawrocki <hdev@sztanski.com>
2022-08-10 17:04:06 -0500

    Fix typo

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::BookingsController < ApplicationController
+class Api::V1::BookingsController < ApplicationController
   before_action :set_booking, only: %i[show update destroy]
 
   # GET /bookings
--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,4 @@
-class API::V1::ItemsController < ApplicationController
+class Api::V1::ItemsController < ApplicationController
   before_action :set_item, only: %i[ show update destroy ]
 
   # GET /items
