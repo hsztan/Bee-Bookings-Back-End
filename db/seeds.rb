f4328aa7fc12a9f251c7a936c52449f28b027e69
Henry Nawrocki <hdev@sztanski.com>
2022-08-10 17:14:24 -0500

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ User.create(username: "Aaron")
+  User.create(username: "Bob")
+  Item.create(name: "Item 1", image: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60", description: "This is item 1")
+  Booking.create(date: "2020-01-01", city: "London", user_id: 1, item_id: 1)
