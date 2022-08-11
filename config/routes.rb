ced8f5b9eaa8a063e0252b9f47d8b7786c5b7104
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-11 15:27:43 -0600

    Removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "signup", to: "sessions#sign_up"
       post "signin", to: "sessions#sign_in"
-      resources :users do
-        resources :bookings
-        resources :items
-      end
+      resources :bookings
+      resources :items
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
