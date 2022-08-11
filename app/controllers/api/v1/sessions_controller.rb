9e4a7ebe1a2141dca00fc78165e51692923d01b8
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-11 15:17:26 -0600

    Sign in method working

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::SessionsController < ApplicationController
   def sign_in
+    @user = User.find_by(username: user_params[:username])
+    if @user
+      render json: { 
+        user_id: @user.id,
+        username: @user.username 
+      }
+    else
+      render json: { error: "User not found" },
+        status: 404
+    end
   end
 
   def sign_up
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
