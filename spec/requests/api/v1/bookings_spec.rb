fd989168d3798c5378405b689327387658c77972
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-12 14:36:04 -0600

    Create bookings test and get bookings by user test running

---
+++ spec/requests/api/v1/bookings_spec.rb
@@ -0,0 +1,86 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/bookings', type: :request do
+  before do
+    @user = User.create(username: "Henry")
+    @user_id = @user.id
+    @item = Item.create(name: "MyItem", image: "MyImage", description: "This is my Item")
+    @item_id = @item.id
+  end
+
+  path '/api/v1/bookings' do
+
+    post('create booking') do
+      tags 'Bookings'
+      consumes 'application/json'
+      parameter name: :booking, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_id: { type: :integer },
+          item_id: { type: :integer },
+          date: { type: :date },
+          city: { type: :string }
+        },
+        required: ['user_id', 'item_id', 'date', 'city']
+      }
+      response(201, 'successful') do
+        let(:booking) { {booking: {user_id: @user_id, item_id: @item_id, date: '12/02/2021', city: 'Tokyo'}} }
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/bookings/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show booking') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete booking') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/bookings/user' do
+
+    post('user booking') do
+      tags 'Bookings'
+      consumes 'application/json'
+      parameter name: :booking, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_id: { type: :integer }
+        },
+        required: ['user_id']
+      }
+      response(200, 'successful') do
+        let(:booking) { {booking: {user_id: @user_id}} }
+        run_test!
+      end
+    end
+  end
+end
