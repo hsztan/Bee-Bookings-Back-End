81707f74f1e5fe38e5768e40a3b6f048499a7834
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-12 11:27:33 -0600

    3 out of 4 items tests passing

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,7 +1,8 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/items', type: :request do
-
+  @item = Item.create(name: "bee", image: "image_url", description: "It's a bee")
+  @id = @item.id
   path '/api/v1/items' do
 
     get('list items') do
@@ -39,11 +40,11 @@ RSpec.describe 'api/v1/items', type: :request do
 
   path '/api/v1/items/{id}' do
     # You'll want to customize the parameter types...
-    parameter name: 'id', in: :path, type: :string, description: 'id'
+    parameter name: 'id', in: :path, type: :integer, description: 'id'
 
     get('show item') do
       response(200, 'successful') do
-        let(:id) { '123' }
+        let(:id) { @id }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -58,15 +59,15 @@ RSpec.describe 'api/v1/items', type: :request do
 
     delete('delete item') do
       response(200, 'successful') do
-        let(:id) { '123' }
+        let(:id) { @id }
 
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+        # after do |example|
+        #   example.metadata[:response][:content] = {
+        #     'application/json' => {
+        #       example: JSON.parse(response.body, symbolize_names: true)
+        #     }
+        #   }
+        # end
         run_test!
       end
     end
